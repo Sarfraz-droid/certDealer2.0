2d1de3342a64ea9eea0855cf820b25f5448a432e
Sarfraz <alamsarfraz422@gmail.com>
2022-04-20 02:52:58 +0530

    CertDealer

--- routes/cert_gen/send_mail.py
+++ routes/cert_gen/send_mail.py
@@ -1,4 +1,6 @@
 import json
+
+from requests import request
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -8,7 +10,23 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 from routes.cert_gen.display_data import generate_cert, CertHandler
+import base64
 
+
+class MailerData:
+  def __init__(self, email, name, subject, body, cert_file):
+    data = cert_file
+    self.email = email
+    self.name = name
+
+    self.subject = subject
+    self.body = body
+    self.attachment = {
+      name: f'{name}.png',
+      data: data
+    }
+  
+    
 
 def mailer(fromaddr,frompass,toaddr,subject,msgbody,file_name,filepath):
   msg = MIMEMultipart()
@@ -29,7 +47,7 @@ def mailer(fromaddr,frompass,toaddr,subject,msgbody,file_name,filepath):
   s.login(fromaddr, frompass)
   text = msg.as_string()
   s.sendmail(fromaddr, toaddr, text)
-  st.write("Mail sent successfully")
+  st.write(f'Mail sent successfully {toaddr}')
   s.quit()
 
 def send_mail(df,result: CertHandler, cert_template: any):
@@ -40,7 +58,7 @@ def send_mail(df,result: CertHandler, cert_template: any):
     st.markdown("2. On Error, [Enable Less Secure Apps](https://myaccount.google.com/lesssecureapps)")
     st.markdown("3. If the error still exists, [Display Unlock Captcha](https://accounts.google.com/b/0/DisplayUnlockCaptcha)")
     
-    email = st.text_input("Enter Email")
+    email = st.text_input("Enter Email", type="password")
     password = st.text_input("Enter Password", type="password")
     subject = st.text_input("Enter Subject")
     body = st.text_area("Enter Body")
@@ -55,4 +73,5 @@ def send_mail(df,result: CertHandler, cert_template: any):
         for i in range(n):
             # pass
             generate_cert(result.head_pos, result.para_pos, head_arr[i], para_arr[i], cert_template, result.fontHead, result.fontPara, result.isRightAligned)
-            mailer(email,password,result.email_arr[i],subject,body, f'{i}.png','./certificate.png')
+        
+            mailer(email,password,result.email_arr[i],subject,body, f'{head_arr[i]}.png','./certificate.png')
